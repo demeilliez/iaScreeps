87b589a911b8f66e3cfe201963741c3da75baabf
demeilliez <c.demeilliez@gmail.com>
2017-05-26 16:20:25 +0200

    Update manager and spawn

--- role.manager.js
+++ role.manager.js
@@ -3,29 +3,21 @@ var roleHarvester = require('role.harvester');
 var roleUpgrader  = require('role.upgrader');
 var roleBuilder   = require('role.builder');
 
-var roomName = Game.spawns['NagotoQg'].room.name;
-
 var moduleManager = {
 	run : function(){
 		//define nbCreeps at 0
-		Memory.nbCreepsHarvester   = 0;
-	    Memory.nbCreepsUpgrader    = 0;
-	    Memory.nbCreepsBuilder     = 0;
+		Memory.nbCreeps   = 0;
+		var needHarvester = 0;
+		var needUpgrader  = 0;
         
 	    //we gave a role for our creeps
 	    for(var name in Game.creeps) {
+	    	Memory.nbCreeps   = Memory.nbCreeps +1;
 	        var creep = Game.creeps[name];
 	        if(creep.memory.role == 'harvester') {
 	            roleHarvester.run(creep);
-	            Memory.nbCreepsHarvester = Memory.nbCreepsHarvester +1;
-	            //Game.rooms[roomName].createConstructionSite(creep.pos, STRUCTURE_ROAD);
 	        }else if(creep.memory.role == 'upgrader') {
 	            roleUpgrader.run(creep);
-	            Memory.nbCreepsUpgrader = Memory.nbCreepsUpgrader +1;
-	            //Game.rooms[roomName].createConstructionSite(creep.pos, STRUCTURE_ROAD);
-	        }else if(creep.memory.role == 'builder'){
-	            roleBuilder.run(creep);
-	            Memory.nbCreepsBuilder = Memory.nbCreepsBuilder +1;
 	        }
 	    }
 	}
